709698f63f808c2c100e21957e5a7387c9d13481
Jagroop-RTE <jagroop@rtesoftwares.com>
2024-04-26 12:24:07 +0530

    Remaining pages done

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,3 +3,24 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 });
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+});
+$(document).ready(function(){
+    $('.copy-btn').click(function(){
+        var textToCopy = $(this).parent().text().trim();
+
+        // Create a temporary textarea element
+        var textarea = $('<textarea>').val(textToCopy).appendTo('body').select();
+
+        // Copy the selected text
+        document.execCommand('copy');
+
+        // Remove the temporary textarea
+        textarea.remove();
+
+        // Change the icon to "done"
+        $(this).html('<span class="material-symbols-outlined">done</span>');
+    });
+});
