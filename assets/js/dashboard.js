29ade30425abddac5c6d1b65794dc661e11a173a
karan-chauhan <karan@askned.com>
2024-04-26 17:21:09 +0530

    Set logo, sidebar and top bar

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+    // $('#sidebarCollapse').on('click', function () {
+    //     $('#sidebar').toggleClass('active');
+    // });
+
+    $(".sidebar-toggle-js").on('click', function () {
+        console.log("thhshs")
+        $('#sidebar').toggleClass('sidebar-active sidebar-inactive');
     });
+
 });
